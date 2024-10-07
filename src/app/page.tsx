f7ce1ef0f7f15a277a191db7bab11b0c690deb66
famgz <famgz@proton.me>
2024-10-07 11:55:05 -0300

    fix: import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { Header } from "@/components/header/Header";
-import'../styles/home.scss'
-import { Experience } from "@/components/experience/Experience";
-import { Information } from "../components/information/Information";
-import { Button } from "../components/button/Button";
+import { Header } from '@/components/header/Header';
+import '../styles/home.scss';
+import { Experience } from '@/components/experience/Experience';
+import { Information } from '../components/information/Information';
+import { Button } from '../components/button/Button';
 
 export default function Home() {
   return (
-      <main className="container">
-        <Header/>
-        <Experience/>
-        <Information/>
-        <Button/>
-      </main>
+    <main className='container'>
+      <Header />
+      <Experience />
+      <Information />
+      <Button />
+    </main>
   );
 }
